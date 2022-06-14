b3aafd27e9f2aead5b3f525c97877b8c348f0873
mayky20 <107127912+mayky20@users.noreply.github.com>
2022-06-14 09:20:51 -0500

    Add files via upload

---
+++ makefile.c
@@ -0,0 +1,6 @@
+README.md:
+	echo "# Juego de Adivinanzas" > README.md
+	echo "" >> README.md
+	echo Creado $$(date +%D) a las $$(date +%H:%M:%S) >> README.md
+	echo "" >> README.md
+	echo Adivina.sh contiene $$(wc -l adivina.sh) lineas >> README.md
